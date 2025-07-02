e25e653960435d3a90849f956aad1b60b4bb8ff4
IDGS-901-22001891 <isaaccrdmc@gmail.com>
2025-07-02 14:19:41 -0600

    Ya he agregado el footer y estoy trabajando en en inicio-compomente, creo que voy a dejar el CSS para el final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabeceraComponentComponent } from './components/cabecera-component/cabecera-component.component';
+import { InicioComponentComponent } from './components/inicio-component/inicio-component.component';
+import { FooterComponentComponent } from './components/footer-component/footer-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CabeceraComponentComponent,
+    InicioComponentComponent,
+    FooterComponentComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
