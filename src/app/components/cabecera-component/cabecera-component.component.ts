e59cc03bc06586d775376e18b676c5bebede6703
IDGS-901-22001891 <isaaccrdmc@gmail.com>
2025-07-02 13:02:13 -0600

    He creado los componentes para ell poryecto, tambien ya esta el bootstrap importado y agregado en el JSON

---
+++ src/app/components/cabecera-component/cabecera-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecera-component',
+  imports: [],
+  templateUrl: './cabecera-component.component.html',
+  styleUrl: './cabecera-component.component.css'
+})
+export class CabeceraComponentComponent {
+
+}
---
+++ src/app/components/listado-productos-component/listado-productos-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-productos-component',
+  imports: [],
+  templateUrl: './listado-productos-component.component.html',
+  styleUrl: './listado-productos-component.component.css'
+})
+export class ListadoProductosComponentComponent {
+
+}
