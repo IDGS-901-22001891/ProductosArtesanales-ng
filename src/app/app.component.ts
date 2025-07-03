2810db9d65bdcf6e8cf6894ac37d4f47838294ce
IDGS-901-22001891 <isaaccrdmc@gmail.com>
2025-07-02 18:35:50 -0600

    Ya he creado el area de los filtros, (Sin CESAR)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { CabeceraComponentComponent } from './components/cabecera-component/cabecera-component.component';
 import { InicioComponentComponent } from './components/inicio-component/inicio-component.component';
 import { FooterComponentComponent } from './components/footer-component/footer-component.component';
+import { ListadoProductosComponentComponent } from "./components/listado-productos-component/listado-productos-component.component";
+import { BuscadorComponentComponent } from "./components/buscador-component/buscador-component.component";
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     CabeceraComponentComponent,
-    InicioComponentComponent,
-    FooterComponentComponent
-  ],
+    // InicioComponentComponent,
+    FooterComponentComponent,
+    ListadoProductosComponentComponent,
+    BuscadorComponentComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
