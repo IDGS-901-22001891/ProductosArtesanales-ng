9a78146ec2797ab59bddb38ff781e6e23e8e1fa6
IDGS-901-22001891 <isaaccrdmc@gmail.com>
2025-07-02 18:16:55 -0600

    He creado el componente del buscador así coo el tema de las pruevas para la segunda pantalla, aún no creo la viculación de estas

---
+++ src/app/components/buscador-component/buscador-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buscador-component',
+  imports: [],
+  templateUrl: './buscador-component.component.html',
+  styleUrl: './buscador-component.component.css'
+})
+export class BuscadorComponentComponent {
+
+}
