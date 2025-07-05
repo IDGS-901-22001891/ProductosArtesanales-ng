25db386f6597c1fc8122604f502166fcef119043
IDGS-901-22001891 <isaaccrdmc@gmail.com>
2025-07-04 21:57:39 -0600

    Agregamos la URL de la API y ahora sigue utilizarlo en el componente

--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Producto } from '../Interfaces/producto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
 
-  constructor() { }
+  private apiUrl = 'https://localhost:7142/Listar';
+
+  constructor(private http: HttpClient) {}
+
+  obtenerProductos(): Observable<Producto[]> {
+    return this.http.get<Producto[]>(this.apiUrl);
+  }
 }
