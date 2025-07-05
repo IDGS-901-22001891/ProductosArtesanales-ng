b63252f02c0055853711551070c1a738a7df7fb8
IDGS-901-22001891 <isaaccrdmc@gmail.com>
2025-07-04 22:07:30 -0600

    Me ha arrojado un error al renderizar, dej alo corrijo

--- src/app/components/listado-productos-component/listado-productos-component.component.ts
+++ src/app/components/listado-productos-component/listado-productos-component.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Producto } from '../../Interfaces/producto';
+import { ProductoService } from '../../services/producto.service';
 
 @Component({
   selector: 'app-listado-productos-component',
@@ -7,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './listado-productos-component.component.css'
 })
 export class ListadoProductosComponentComponent {
+  productos: Producto[] = [];
+
+  constructor(private productoService: ProductoService) {}
+
+  ngOnInit(): void {
+    this.productoService.obtenerProductos().subscribe(
+      data => {
+        this.productos = data;
+      },
+      error => {
+        console.error('Error al obtener productos:', error);
+      }
+    );
+  }
+
 
 }
+
