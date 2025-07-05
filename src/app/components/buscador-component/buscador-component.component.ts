40d4894441c68ea2ee1cea10d56de9976d14d7f2
IDGS-901-22001891 <isaaccrdmc@gmail.com>
2025-07-04 22:26:21 -0600

    Empezamos con los filtros

--- src/app/components/buscador-component/buscador-component.component.ts
+++ src/app/components/buscador-component/buscador-component.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-buscador-component',
@@ -7,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrl: './buscador-component.component.css'
 })
 export class BuscadorComponentComponent {
+
+  // ? Creamos la seccion para los filtros y la busqueda
+  @Output() filtrosCambiados = new EventEmitter<string[]>();
+  @Output() textoBusquedaCambiado = new EventEmitter<string>();
+
+
+  filtrosSeleccionados: string[] = [];
+
+
+  onCheckboxChange(categoria: string, event: Event) {
+    const isChecked = (event.target as HTMLInputElement).checked;
+    if (isChecked) {
+      this.filtrosSeleccionados.push(categoria);
+    } else {
+      this.filtrosSeleccionados = this.filtrosSeleccionados.filter(c => c !== categoria);
+    }
+    this.filtrosCambiados.emit(this.filtrosSeleccionados);
+  }
+
+  onInputChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.textoBusquedaCambiado.emit(value);
+  }
+
 
 }
