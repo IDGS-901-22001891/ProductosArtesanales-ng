ffcc80329c35ba002c3ad3f930aa088c902b4dbf
IDGS-901-22001891 <isaaccrdmc@gmail.com>
2025-07-04 22:31:34 -0600

    Por ahí va

--- src/app/components/listado-productos-component/listado-productos-component.component.ts
+++ src/app/components/listado-productos-component/listado-productos-component.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { Producto } from '../../Interfaces/producto';
 import { ProductoService } from '../../services/producto.service';
 import { CommonModule } from '@angular/common';
+import { BuscadorComponentComponent } from '../buscador-component/buscador-component.component';
 
 @Component({
   selector: 'app-listado-productos-component',
   standalone: true, // ? Agregamos el decorador de standalone
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    BuscadorComponentComponent
+  ],
   templateUrl: './listado-productos-component.component.html',
   styleUrl: './listado-productos-component.component.css'
 })
@@ -14,6 +18,15 @@ export class ListadoProductosComponentComponent {
 
   // ? Creamos un array de productos
   productos: Producto[] = [];
+
+  // ? Creamso un nuevo array para los ffiltros:
+  productosFiltrados: Producto[] = [];
+
+
+  filtrosActivos: string[] = [];
+  textoBusqueda: string = '';
+
+
 
   // ? Inyectamos el servicio de productos
   constructor(private productoService: ProductoService) {}
@@ -23,6 +36,7 @@ export class ListadoProductosComponentComponent {
     this.productoService.obtenerProductos().subscribe(
       data => {
         this.productos = data;
+        this.productosFiltrados = data;
       },
       error => {
         console.error('Error al obtener productos:', error);
@@ -31,5 +45,25 @@ export class ListadoProductosComponentComponent {
   }
 
 
+  onFiltrosCambiados(filtros: string[]) {
+    this.filtrosActivos = filtros;
+    this.filtrarProductos();
+  }
+
+  onTextoBusquedaCambiado(texto: string) {
+    this.textoBusqueda = texto.toLowerCase();
+    this.filtrarProductos();
+  }
+
+  filtrarProductos() {
+    this.productosFiltrados = this.productos.filter(p => {
+      const cumpleFiltroCategoria =
+      this.filtrosActivos.length === 0 ||
+      p.categorias.some(cat => this.filtrosActivos.includes(cat));
+      const cumpleBusqueda =
+        !this.textoBusqueda || p.name.toLowerCase().includes(this.textoBusqueda) || p.description.toLowerCase().includes(this.textoBusqueda);
+      return cumpleFiltroCategoria && cumpleBusqueda;
+    });
+  }
 }
 
