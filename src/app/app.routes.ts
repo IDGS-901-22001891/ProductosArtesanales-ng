2c01e77929f95153d9a94de79bc4e023eae884a7
IDGS-901-22001891 <isaaccrdmc@gmail.com>
2025-07-04 23:16:01 -0600

    Creando la navegación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponentComponent } from './components/inicio-component/inicio-component.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InicioComponentComponent },
+  { path: 'productos', component: InicioComponentComponent },
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
